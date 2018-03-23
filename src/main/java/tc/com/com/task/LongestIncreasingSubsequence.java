dfd2243054a64df152f93620711ef04982a182c6
Tanchuan <bjtanchuan@corp.netease.com>
2018-03-23 23:29:33 +0800

    最长递增子序列方法二

--- src/main/java/tc/com/com/task/LongestIncreasingSubsequence.java
+++ src/main/java/tc/com/com/task/LongestIncreasingSubsequence.java
@@ -10,6 +10,9 @@ public class LongestIncreasingSubsequence {
 
 
     public static int lengthOfLIS1(int[] nums) {
+        if(null == nums || nums.length <= 0){
+            return 0;
+        }
         int[] minMaxV = new int[nums.length + 1];
         int[] lenOfSeq = new int[nums.length];
         minMaxV[0] =  CommonUtil.min(nums) - 1;//哨兵
@@ -41,12 +44,40 @@ public class LongestIncreasingSubsequence {
 
 
     public static int lengthOfLIS2(int[] nums) {
-        return 0;
+        if(null == nums || nums.length <= 0){
+            return 0;
+        }
+        int maxIndex = -1;
+        int maxLen = 1;
+        int[] prev = new int[nums.length];
+        int[] lengths = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++){
+            lengths[i] = 1;
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i] && lengths[j] + 1 > lengths[i]){
+                    lengths[i] = lengths[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if(lengths[i] > maxLen){
+                maxLen = lengths[i];
+                maxIndex = i;
+            }
+        }
+
+        int leftCount = maxLen;
+        for(; leftCount > 0; maxIndex = prev[maxIndex], leftCount--){
+            System.out.print(" " + nums[maxIndex]);
+        }
+        System.out.println();
+        return maxLen;
     }
 
     public static void main(String[] args){
         int[] arr = {1, -1, 2, -3, 4, -5, 6, -7};
         System.out.println(lengthOfLIS1(arr));
+        System.out.println(lengthOfLIS2(arr));
     }
 
 }
