de7b32987b80b0bdd5c5694752a2ce99ae5f9876
Tanchuan <bjtanchuan@corp.netease.com>
2018-05-17 18:10:40 +0800

    跳表初步

---
+++ src/main/java/tc/com/com/task/SkipList.java
@@ -0,0 +1,35 @@
+package tc.com.com.task;
+
+/**
+ * 跳表
+ */
+public class SkipList {
+    class Node{
+        int key;
+        int value;
+        Node[] forwards;
+
+    }
+
+    Node head, nil;
+
+
+    private void init(){
+
+    }
+
+    public Node search(int key){
+        return null;
+    }
+
+    public boolean add(int key, int val){
+        return false;
+    }
+
+    public boolean delete(int key){
+        return false;
+    }
+
+
+
+}
